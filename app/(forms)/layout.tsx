5654f0933062f58afd7ecc6decbc59ed46f5e92d
PedroHenriqueMA <phenriquemandrade@hotmail.com>
2024-12-19 20:53:45 -0300

    form page commit

--- app/(forms)/layout.tsx
+++ app/(forms)/layout.tsx
@@ -6,9 +6,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex h-[100vh] flex-col items-center justify-center ">
       <Image priority={true} width={150} height={50} src={'/logo/logo-white.png'} alt="chaty logo" />
-      <div>{children}</div>
+      {children}
     </div>
   );
 }
