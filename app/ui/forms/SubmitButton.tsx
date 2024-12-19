db87b1abf1bdbe94dab154f408f430497aa8687d
PedroHenriqueMA <phenriquemandrade@hotmail.com>
2024-12-19 20:01:35 -0300

    vercel fix(again)

--- app/ui/forms/SubmitButton.tsx
+++ app/ui/forms/SubmitButton.tsx
@@ -3,7 +3,8 @@
 import { Inter } from "next/font/google";
 
 const inter = Inter({
-    weight:"700"
+    weight:"700",
+    subsets:["latin"]
 })
 export default function SubmitButton(param:{
     title: string,
